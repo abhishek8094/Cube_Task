4374f7dd19f74cca3d0f352e2b428fc05ad2d816
abhisek8094 <iasabhi41@gmail.com>
2024-08-03 22:20:14 +0530

    task ho gya

---
+++ src/CustomerPhotoGrid.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+interface CustomerPhotoGridProps {
+  images: string[];
+}
+
+const CustomerPhotoGrid: React.FC<CustomerPhotoGridProps> = ({ images }) => {
+  return (
+    <div className="customer-photo-grid">
+      {images.map((image) => (
+        <img key={image} src={image} alt="Customer photo" />
+      ))}
+    </div>
+  );
+};
+
+export default CustomerPhotoGrid;
